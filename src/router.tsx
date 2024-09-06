5726f8a3b35d08a4614cdcea5d7a686d3a75009d
hoseon07 <a01081930429@gmail.com>
2024-09-06 11:03:55 +0900

    ✨:: 메인페이지 급식 정보 컴포넌트 개발

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,23 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./Styles/GlobalStyle.style";
+import MealTime from "./Components/MealTime/MealTime";
+import BreakfastList from "./Components/MealList/BreakfastList";
+import LunchList from "./Components/MealList/LunchList";
+import DinnerList from "./Components/MealList/DinnerList";
+import AllergyNotification from "./Components/AllergyNotification/AllergyNotification";
+import NoAllergy from "./Components/AllergyNotification/NoAllergy";
 
 function MainRouter() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path="" element="" />
+        <Route path="/MealTime" element={<MealTime />} />
+        <Route path="/BreakfastList" element={<BreakfastList />} />
+        <Route path="/LunchList" element={<LunchList />} />
+        <Route path="/DinnerList" element={<DinnerList />} />
+        <Route path="/AllergyNotification" element={<AllergyNotification />} />
+        <Route path="/NoAllergy" element={<NoAllergy />} />
       </Routes>
     </BrowserRouter>
   );
