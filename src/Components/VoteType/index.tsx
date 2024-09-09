efb43091f04a52d171f01451ee29c620c1df52ee
heedda <heedda.kollim@gmail.com>
2024-09-09 19:05:05 +0900

    ♻️ :: string => boolean

--- src/Components/VoteType/index.tsx
+++ src/Components/VoteType/index.tsx
@@ -3,31 +3,30 @@ import * as S from "./style";
 import { Font } from "../../Styles/Font";
 
 const VoteType = () => {
-  const [selectType, setSelectType] = useState<string>("ing");
+  const [selectType, setSelectType] = useState<boolean>(true);
 
   const handleSelectType = (type: string) => {
-    setSelectType(type);
+    if (type === "ing") {
+      setSelectType(true);
+    } else setSelectType(false);
   };
   return (
     <S.VoteTypeContainer>
-      <S.TypeOfVote
-        select={selectType === "ing"}
-        onClick={() => handleSelectType("ing")}
-      >
+      <S.TypeOfVote select={selectType} onClick={() => handleSelectType("ing")}>
         <Font
           text="현재 진행 중인 투표"
           kind="label2"
-          color={selectType === "ing" ? "white" : "black"}
+          color={selectType ? "white" : "black"}
         />
       </S.TypeOfVote>
       <S.TypeOfVote
-        select={selectType === "end"}
+        select={!selectType}
         onClick={() => handleSelectType("end")}
       >
         <Font
           text="종료된 투표"
           kind="label2"
-          color={selectType === "end" ? "white" : "black"}
+          color={!selectType ? "white" : "black"}
         />
       </S.TypeOfVote>
     </S.VoteTypeContainer>
