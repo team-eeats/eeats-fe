b9b86efae34287c4933f49bea312fddb3b8a7554
heedda <heedda.kollim@gmail.com>
2024-09-10 11:59:52 +0900

    ✨ :: 버튼 컴포넌트 만들기

---
+++ src/Components/Button/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { Color } from "../../Styles/Color";
+
+interface ButtonContainerPropsType {
+  activate: boolean;
+  available: boolean;
+}
+
+export const ButtonContainer = styled.button<ButtonContainerPropsType>`
+  width: 138px;
+  height: 55px;
+  border-radius: 16px;
+  border: none;
+  background-color: ${(props) =>
+    props.available
+      ? props.activate
+        ? Color.main300
+        : Color.main100
+      : Color.main500};
+  cursor: pointer;
+`;
