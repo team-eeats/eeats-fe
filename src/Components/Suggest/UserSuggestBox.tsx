6b794aef59f6be7b9d0bc419f8823379feb7bd11
limda00 <dylim7715@gmail.com>
2024-09-11 21:07:09 +0900

    💄 :: 사용자 급식 건의 메세지 컴포넌트 퍼블리싱

---
+++ src/Components/Suggest/UserSuggestBox.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import * as S from "./index"
+import { Font } from "../../Styles/Font"
+import Setting from "../../assets/img/Setting.svg"
+import ArrowRight from "../../assets/img/ArrowRight.svg"
+
+/**
+ * 
+ * @returns 급식 건의하기 학생 건의 컴포넌트
+ */
+
+const UserSuggestBox = () => {
+
+    const [selected, setSelected] = useState(false)
+
+    return (
+        <S.Container onClick={() => setSelected(!selected)} selected={selected}>
+            <S.TopWrap>
+                <S.NicknameAndDateWrap>
+                    <Font text="멋진토마토" kind="Body1" color="main200" />
+                    <Font text="8월 28일" kind="Body1" color="gray300" />
+                </S.NicknameAndDateWrap>
+                <img src={Setting} alt="더보기" />
+            </S.TopWrap>
+
+            <Font text="선생님 밥이 사악 맛있어요ㅜㅜ!" kind="Heading3" />
+
+            <Font text="선생님 밥이 사악 맛있어요ㅜㅜ!선생님 밥이 사악 맛있어요ㅜㅜ!선생님 밥이 사악 맛있어요ㅜㅜ!" kind="Body2" color="gray600" />
+
+            {
+                selected ?
+                    (
+                        <S.Comment>
+                            <img src={ArrowRight} alt="화살표" />
+                            <S.Input placeholder="답변 대기 중입니다." readOnly />
+                        </S.Comment>
+                    ) : (<></>)
+            }
+
+        </S.Container>
+    )
+}
+
+export default UserSuggestBox
