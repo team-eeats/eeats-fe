446af899d78dea7a33980deb801da2d321929166
은지 <wej0513@dsm.hs.kr>
2024-09-11 19:21:11 +0900

    ✨:: 알림 모달 퍼블리싱

--- src/router.tsx
+++ src/router.tsx
@@ -11,6 +11,7 @@ import TabBar from "./Components/TabNavigation/TabBar";
 import DonutGraph from "./Components/DonutGraph";
 import { CompletePhotoVote } from "./Components/CompletePhotoVote";
 import { Header } from "./Components/Header";
+import { AlarmModal } from "./Components/AlarmModal";
 
 function router() {
   return (
@@ -28,6 +29,7 @@ function router() {
         <Route path="/NoAllergy" element={<NoAllergy />} />
         <Route path="/Header" element={<Header />} />
         <Route path="/tab" element={<TabBar />} />
+        <Route path="/AlarmModal" element={<AlarmModal />} />
       </Routes>
     </BrowserRouter>
   );
