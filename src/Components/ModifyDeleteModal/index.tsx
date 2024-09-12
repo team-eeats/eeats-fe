90bca999e5b21617ec10d2ab97f5d9f355ecc4e7
limda00 <dylim7715@gmail.com>
2024-09-12 14:14:21 +0900

    ✨ :: 수정 버튼 클릭시 페이지 이동

--- src/Components/ModifyDeleteModal/index.tsx
+++ src/Components/ModifyDeleteModal/index.tsx
@@ -1,11 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import { Font } from '../../Styles/Font';
 import * as S from './style'
 
 const ModifyDeleteModal = () => {
+  const navigate = useNavigate()
+
   return (
     <S.Container>
       <S.Modal>
-        <S.TextWrap>
+        <S.TextWrap onClick={() => navigate("/suggestModify")}>
           <Font kind="Button1" text="수정하기" />
         </S.TextWrap>
         <S.Line></S.Line>
