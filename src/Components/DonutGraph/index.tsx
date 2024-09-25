e235ddc8faadfa2d4b04ca65a6dd71093cb7bb2b
limda00 <dylim7715@gmail.com>
2024-09-25 19:15:35 +0900

    💄 :: 메뉴 랭킹 결과 퍼블리싱

--- src/Components/DonutGraph/index.tsx
+++ src/Components/DonutGraph/index.tsx
@@ -2,21 +2,33 @@ import * as S from "./styled";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 import { Color } from "../../Styles/Color";
+import { Font } from "../../Styles/Font";
+import Crown from "../../assets/img/SVG/Crown.svg"
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
+const result = [
+  { menu: "꼬사삭 치킨", percent: 78, king: true },
+  { menu: "국물 떡볶이", percent: 16, king: false },
+  { menu: "청국장", percent: 12, king: false },
+  { menu: "기타", percent: 6, king: false },
+]
+
+const labelColor = ["#FF583B", "#2784FF", "#C707F2", "#959595"]
+
 export default function DonutGraph() {
   const Data = {
-    labels: ["꼬사삭 치킨", "국물 떡볶이", "청국장"],
+    labels: result.map(item => item.menu),
     datasets: [
       {
         data: [40, 25, 35],
-        backgroundColor: [`${Color.main500}`, "#b5f2ff", "#c5f2ba"],
+        backgroundColor: ["#FF9B8A", "#959595", "#D2D2D2"],
         borderColor: "#FFFFFF",
         borderWidth: 5,
       },
     ],
   };
+
   const Options = {
     plugins: {
       tooltip: {
@@ -37,9 +49,33 @@ export default function DonutGraph() {
     },
     cutout: "65%", // 도넛 차트의 비율
   };
+
   return (
     <S.Container>
-      <Doughnut data={Data} options={Options}></Doughnut>
+      <S.DoughnutContent>
+        <S.Hide></S.Hide>
+        <Doughnut data={Data} options={Options}></Doughnut>
+        <S.VoteNumber>
+          <Font text="투표인원" kind="Label3" color="gray400" />
+          <Font text="259명" kind="Heading2" />
+        </S.VoteNumber>
+      </S.DoughnutContent>
+
+      <S.ResultWrap>
+        <Font text="전체결과" kind="Body1" color="gray300" />
+        {
+          result.map((value, index) => (
+            <S.MenuAndPercent>
+              <S.MenuWrap>
+                <S.PointLine style={{ backgroundColor: labelColor[index] }}></S.PointLine>
+                <Font text={value.menu} kind="Heading3" color="gray600" />
+                {value.king ? <S.Image src={Crown} /> : null}
+              </S.MenuWrap>
+              <Font text={`${value.percent}%`} kind="Body1" color="gray600" />
+            </S.MenuAndPercent>
+          ))
+        }
+      </S.ResultWrap>
     </S.Container>
   );
 }
