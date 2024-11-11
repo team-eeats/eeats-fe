59abd40966240718213d97210b481c99e08bbc3e
은지 <wej0513@dsm.hs.kr>
2024-11-11 22:04:07 +0900

    ✨ :: 건의 만들기 api 연동

--- src/Pages/Suggest/Upload.tsx
+++ src/Pages/Suggest/Upload.tsx
@@ -1,19 +1,38 @@
-import { useState } from 'react';
-import * as S from './style'
-import { Font } from '../../Styles/Font';
-import Button from '../../Components/Button';
+import { useState } from "react";
+import * as S from "./style";
+import { Font } from "../../Styles/Font";
+import Button from "../../Components/Button";
+import { CreateSuggest } from "../../Apis/suggestions";
+import { useNavigate } from "react-router-dom";
 
 const Upload = () => {
-  const [inputCount, setInputCount] = useState<number>(0)
-  const [excessLimit, setExcessLimit] = useState<boolean>(false)
+  const [inputCount, setInputCount] = useState<number>(0);
+  const [excessLimit, setExcessLimit] = useState<boolean>(false);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const navigate = useNavigate();
 
   const onInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
     setInputCount(e.target.value.length);
     if (inputCount > 30) {
-      return setExcessLimit(true)
+      return setExcessLimit(true);
     }
-    setExcessLimit(false)
-  }
+    setExcessLimit(false);
+  };
+
+  const handleCreateSuggest = async () => {
+    try {
+      await CreateSuggest({
+        title: title,
+        content: content,
+      });
+      console.log("게시글이 성공적으로 작성되었습니다!");
+      navigate("/suggest");
+    } catch (error) {
+      console.error("게시물 생성 오류:", error);
+    }
+  };
 
   return (
     <S.Container>
@@ -25,23 +44,30 @@ const Upload = () => {
           <S.Write>
             <S.LabelWrap>
               <Font text="제목" kind="Label2" />
-              <S.Input placeholder="제목을 작성해주세요"></S.Input>
+              <S.Input
+                placeholder="제목을 작성해주세요"
+                onChange={(e) => setTitle(e.target.value)}
+              ></S.Input>
             </S.LabelWrap>
             <S.LabelWrap>
               <Font text="건의 내용" kind="Label2" />
               <S.TextAreaWrap>
                 <S.TextArea placeholder="건의 내용을 작성해주세요" onChange={(e) => onInputHandler(e)}></S.TextArea>
                 <S.CharacterLimitation>
-                  <Font text={`${inputCount}/30`} kind="Caption2" color={`${excessLimit ? "main300" : "gray300"}`} />
+                  <Font
+                    text={`${inputCount}/30`}
+                    kind="Caption2"
+                    color={`${excessLimit ? "main300" : "gray300"}`}
+                  />
                 </S.CharacterLimitation>
               </S.TextAreaWrap>
             </S.LabelWrap>
           </S.Write>
         </S.ContentBox>
-        <Button text="건의하기" activate onClick={() => { }} />
+        <Button text="건의하기" activate onClick={handleCreateSuggest} />
       </S.SubmitContent>
     </S.Container>
-  )
-}
+  );
+};
 
 export default Upload;
