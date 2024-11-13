d32ea02375734b37793372408541c42ffe036c32
은지 <wej0513@dsm.hs.kr>
2024-11-13 13:28:47 +0900

    ✨ :: 알림 api 연동

--- src/Components/AlarmModal/index.tsx
+++ src/Components/AlarmModal/index.tsx
@@ -1,22 +1,66 @@
+import { useEffect, useState } from "react";
 import { Font } from "../../Styles/Font";
 import * as S from "./styled";
+import { GetAlarmList } from "../../Apis/notifications/type";
+import { AlarmList, ReadAlarm } from "../../Apis/notifications";
+import { timeForToday } from "../../utils/time";
 
 type AlarmModalProps = {
   onClose: () => void;
 };
 
 export const AlarmModal: React.FC<AlarmModalProps> = () => {
+  const [alarm, setAlarm] = useState<GetAlarmList | null>(null);
+
+  useEffect(() => {
+    const handleAlarm = async () => {
+      try {
+        const response = await AlarmList();
+        setAlarm(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    handleAlarm();
+  }, []);
+
+  const handleRead = async (id: string) => {
+    try {
+      await ReadAlarm(id);
+      setAlarm((prevAlarm) => {
+        if (!prevAlarm) return prevAlarm;
+        return {
+          ...prevAlarm,
+          notifications: prevAlarm.notifications.map((notification) =>
+            notification.id === id
+              ? { ...notification, isRead: true }
+              : notification
+          ),
+        };
+      });
+    } catch (error) {
+      console.error("Failed to mark as read:", error);
+    }
+  };
+
   return (
     <S.Container>
       <Font text="알림" kind="SubTitle2" />
       <S.AlarmWrapper>
-        <S.Alarm>
-          <S.Title>
-            <Font text="오늘 급식에 생선 나와요. 주의하세요!" kind="Label2" />
-            <S.Check></S.Check>
-          </S.Title>
-          <Font text="1일전" kind="Label3" color="gray600" />
-        </S.Alarm>
+        {alarm?.notifications.map((value) => (
+          <S.Alarm key={value.id} onClick={() => handleRead(value.id)}>
+            <S.Title>
+              <Font text={value.title} kind="Label2" />
+              {!value.isRead && <S.Check />}
+            </S.Title>
+            <Font
+              text={timeForToday(value.createdAt)}
+              kind="Label3"
+              color="gray600"
+            />
+          </S.Alarm>
+        ))}
       </S.AlarmWrapper>
     </S.Container>
   );
