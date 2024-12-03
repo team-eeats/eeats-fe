22fba13d03c6cc4e269a0c01c6aeff0e4673a92f
은지 <wej0513@dsm.hs.kr>
2024-12-03 10:25:26 +0900

    ✨ :: 알러지 경고 문구 띄우기 api 연동

--- src/Components/AllergyNotification/AllergyNotification.tsx
+++ src/Components/AllergyNotification/AllergyNotification.tsx
@@ -1,27 +1,61 @@
+import { useEffect, useState } from "react";
 import { Font } from "../../Styles/Font";
 import * as S from "./Styled";
-
-interface MealListProps {
-  selectedMeal: string;
-}
+import { AllergyWarning } from "../../Apis/allergy";
+import { Color } from "../../Styles/Color";
 
 /**
  *
  * @returns 해당 급식에 포함된 알레르기 정보를 알려주는 컴포넌트
  */
 
-const AllergyNotification = ({ selectedMeal }: MealListProps) => {
+const AllergyNotification = () => {
+  const [allergy, setAllergy] = useState<string>("");
+
+  useEffect(() => {
+    const TodayAllergy = async () => {
+      try {
+        // const today = new Date();
+        // const formattedDate = today
+        //   .toISOString()
+        //   .slice(0, 10)
+        //   .replace(/-/g, "");
+        const response = await AllergyWarning("20241123");
+        setAllergy(response.data);
+      } catch (error) {
+        console.error("데이터 로딩 오류:", error);
+      }
+    };
+    TodayAllergy();
+  }, []);
+
   return (
     <>
       <S.Container>
         <Font
-          text={`오늘 ${selectedMeal}에서 나의 알레르기는?`}
+          text="오늘 급식에서 나의 알레르기는?"
           kind="Label2"
           color="gray400"
         />
         <S.SubTitle>
-          <S.ImpactText>{"MealName"}</S.ImpactText>에<br />
-          <S.ImpactText>{"Allergy"}</S.ImpactText> 알레르기 성분이 있어요!
+          {allergy.split(/(에|알레르기 성분이 있어요!)/).map((part, index) => {
+            if (part === "에") {
+              return (
+                <span key={index}>
+                  {part}
+                  <br />
+                </span>
+              );
+            }
+            if (part === "알레르기 성분이 있어요!") {
+              return <span key={index}>{part}</span>;
+            }
+            return (
+              <span key={index} style={{ color: Color.main400 }}>
+                {part}
+              </span>
+            );
+          })}
         </S.SubTitle>
       </S.Container>
     </>
